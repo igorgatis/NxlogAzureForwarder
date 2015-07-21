d4dc83316961e2130ccae0b95c592752cf811462
Igor Gatis <igorgatis@gmail.com>
2015-07-21 14:55:13 -0300

    Installation logs now appear under C:\Logs\NxlogAzureForwarder.

--- AzurePlugin/NxlogAzureForwarder/install-plugin.cmd
+++ AzurePlugin/NxlogAzureForwarder/install-plugin.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM Needed redirect to powershell command line because VS checks for existence of task's commandLine file.
-set ROOTDIR=%~dp0
-set LOGFILE=%ROOTDIR%\install-log.txt
-PowerShell.exe -ExecutionPolicy UnRestricted -File %ROOTDIR%\install-plugin.ps1 > %LOGFILE% 2>&1
+set LOGDIR=C:\Logs\NxlogAzureForwarder
+mkdir %LOGDIR%
+PowerShell.exe -ExecutionPolicy UnRestricted -File "%~dp0\install-plugin.ps1" > "%LOGDIR%\install-log.txt" 2>&1
 exit /b 0
