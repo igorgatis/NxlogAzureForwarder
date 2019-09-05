05b83225eddc70e8cba25469c152d39b3e8d04f2
Igor Gatis <igorgatis@gmail.com>
2019-09-05 15:17:06 -0300

    Added support for multiple queues and tables.

--- src/NxlogAzureForwarder/NxlogAzureForwarderService.cs
+++ src/NxlogAzureForwarder/NxlogAzureForwarderService.cs
@@ -9,68 +9,72 @@ using System.ServiceProcess;
 
 namespace NxlogAzureForwarder
 {
-    public partial class NxlogAzureForwarderService : ServiceBase
-    {
-        private const string kConnectionString = "ConnectionString";
-        private const string kQueueName = "QueueName";
-        private const string kTableName = "TableName";
-        private const string kPartitionKey = "PartitionKey";
-        private const string kRowKey = "RowKey";
-        private const string kAditionalColumns = "AditionalColumns";
-        private const string kDebug = "Debug";
+	public partial class NxlogAzureForwarderService : ServiceBase
+	{
+		private const string kConnectionString = "ConnectionString";
+		private const string kQueueName = "QueueName";
+		private const string kTableName = "TableName";
+		private const string kPartitionKey = "PartitionKey";
+		private const string kRowKey = "RowKey";
+		private const string kAditionalColumns = "AditionalColumns";
+		private const string kDebug = "Debug";
 
-        private HttpServer _server;
+		private HttpServer _server;
 
-        public NxlogAzureForwarderService()
-        {
-            InitializeComponent();
-        }
+		public NxlogAzureForwarderService()
+		{
+			InitializeComponent();
+		}
 
-        protected override void OnStart(string[] args)
-        {
-            try
-            {
-                var appSettings = ConfigurationManager.AppSettings;
-                var account = CloudStorageAccount.Parse(appSettings[kConnectionString]);
-                bool debug = false;
-                if (!bool.TryParse(appSettings[kDebug], out debug)) debug = false;
-                var options = new Uploader.Options
-                {
-                    QueueName = appSettings[kQueueName],
-                    TableName = appSettings[kTableName],
-                    PartitionKey = appSettings[kPartitionKey],
-                    RowKey = appSettings[kRowKey],
-                    AditionalColumns = new HashSet<string>(appSettings[kAditionalColumns].Split(',')),
-                    Debug = debug,
-                };
+		private static string[] Split(string text)
+		{
+			return (text ?? "").Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+		}
 
-                Trace.TraceInformation(JsonConvert.SerializeObject(options));
-                if (string.IsNullOrEmpty(options.TableName) &&
-                    string.IsNullOrEmpty(options.QueueName))
-                {
-                    throw new Exception("No output specified.");
-                }
+		protected override void OnStart(string[] args)
+		{
+			try
+			{
+				var appSettings = ConfigurationManager.AppSettings;
+				var account = CloudStorageAccount.Parse(appSettings[kConnectionString]);
+				bool debug = false;
+				if (!bool.TryParse(appSettings[kDebug], out debug)) debug = false;
+				var options = new Uploader.Options
+				{
+					QueueNames = Split(appSettings[kQueueName]),
+					TableNames = Split(appSettings[kTableName]),
+					PartitionKey = appSettings[kPartitionKey],
+					RowKey = appSettings[kRowKey],
+					AditionalColumns = new HashSet<string>(appSettings[kAditionalColumns].Split(',')),
+					Debug = debug,
+				};
 
-                _server = new HttpServer(Dns.GetHostName(), new Uploader(account, options), debug);
-                _server.Start();
-            }
-            catch (Exception e)
-            {
-                Trace.TraceError(e.ToString());
-                Stop();
-            }
-        }
+				Trace.TraceInformation(JsonConvert.SerializeObject(options));
+				if (options.TableNames.Length == 0 && options.QueueNames.Length == 0)
+				{
+					throw new Exception("No output specified.");
+				}
 
-        protected override void OnStop()
-        {
-            try
-            {
-                if (_server != null) _server.Stop();
-            }
-            catch (Exception e)
-            {
-                Trace.TraceError(e.ToString());
-            }
-        }
-    }
+				_server = new HttpServer(Dns.GetHostName(), new Uploader(account, options), debug);
+				_server.Start();
+			}
+			catch (Exception e)
+			{
+				Trace.TraceError(e.ToString());
+				Stop();
+			}
+		}
+
+		protected override void OnStop()
+		{
+			try
+			{
+				if (_server != null) _server.Stop();
+			}
+			catch (Exception e)
+			{
+				Trace.TraceError(e.ToString());
+			}
+		}
+	}
 }
--- src/NxlogAzureForwarder/Uploader.cs
+++ src/NxlogAzureForwarder/Uploader.cs
@@ -8,127 +8,180 @@ using System.Diagnostics;
 
 namespace NxlogAzureForwarder
 {
-    internal class Uploader
-    {
-        public class Options
-        {
-            public string QueueName { get; set; }
-            public string TableName { get; set; }
-            public string PartitionKey { get; set; }
-            public string RowKey { get; set; }
-            public HashSet<string> AditionalColumns { get; set; }
-            public bool Debug { get; set; }
-        }
+	internal class Uploader
+	{
+		public class Options
+		{
+			public string[] TableNames { get; set; }
+			public string[] QueueNames { get; set; }
 
-        private CloudStorageAccount _account;
-        private Options _options;
-        private CloudTable _table;
-        private CloudQueue _queue;
+			public string PartitionKey { get; set; }
+			public string RowKey { get; set; }
+			public HashSet<string> AditionalColumns { get; set; }
+			public bool Debug { get; set; }
+		}
 
-        public Uploader(CloudStorageAccount account, Options options)
-        {
-            _account = account;
-            _options = options;
-        }
+		private TableUploader _tables;
+		private QueueUploader _queues;
 
-        public bool Upload(LogRecord record)
-        {
-            int expectation = 0;
-            int actual = 0;
-            try
-            {
-                if (_table == null && !string.IsNullOrEmpty(_options.TableName))
-                {
-                    _table = _account.CreateCloudTableClient()
-                        .GetTableReference(_options.TableName);
-                    _table.CreateIfNotExists();
-                }
-                if (_table != null)
-                {
-                    expectation += 1;
-                    var entity = ConvertToEntity(record);
-                    var result = _table.Execute(TableOperation.InsertOrReplace(entity));
-                    var statusGroup = result.HttpStatusCode / 100;
-                    if (statusGroup != 2 && statusGroup != 4)
-                    {
-                        if (_options.Debug) Trace.TraceError("HTTP status: " + result.HttpStatusCode);
-                        return false;
-                    }
-                    actual += 1;
-                }
-            }
-            catch (Exception e)
-            {
-                _table = null;
-                if (_options.Debug) Trace.TraceError(e.ToString());
-                return false;
-            }
+		public Uploader(CloudStorageAccount account, Options options)
+		{
+			_tables = new TableUploader(account, options.TableNames,
+				options.PartitionKey, options.RowKey, options.AditionalColumns, options.Debug);
+			_queues = new QueueUploader(account, options.QueueNames, options.Debug);
+		}
 
-            try
-            {
-                if (_queue == null && !string.IsNullOrEmpty(_options.QueueName))
-                {
-                    _queue = _account.CreateCloudQueueClient()
-                        .GetQueueReference(_options.QueueName);
-                    _queue.CreateIfNotExists();
-                }
-                if (_queue != null)
-                {
-                    expectation += 2;
-                    _queue.AddMessage(Serialize(record));
-                    actual += 2;
-                }
-            }
-            catch (Exception e)
-            {
-                _queue = null;
-                if (_options.Debug) Trace.TraceError(e.ToString());
-                return false;
-            }
+		internal bool Upload(LogRecord record)
+		{
+			var tableOk = _tables.Upload(record);
+			var queueOk = _queues.Upload(record);
+			return tableOk && queueOk;
+		}
 
-            if (_options.Debug && actual != expectation)
-            {
-                Trace.TraceError(string.Format("expectation={0} != {1}=actual", expectation, actual));
-            }
+		private abstract class RoundRobinUploader<TCollection>
+			where TCollection : class
+		{
+			protected CloudStorageAccount _account;
+			protected string[] _names;
+			protected bool _debug;
 
-            return expectation == actual;
-        }
+			private TCollection[] _list;
+			private int _index;
 
-        private CloudQueueMessage Serialize(LogRecord message)
-        {
-            var lean = new LogRecord
-            {
-                Origin = message.Origin,
-                EventTimestamp = message.EventTimestamp,
-                RawData = message.RawData ?? "",
-            };
-            return new CloudQueueMessage(JsonConvert.SerializeObject(lean));
-        }
+			public bool Enabled { get { return _list.Length > 0; } }
 
-        private DynamicTableEntity ConvertToEntity(LogRecord record)
-        {
-            var entity = new DynamicTableEntity
-            {
-                PartitionKey = PropertyRender.Render(_options.PartitionKey, record),
-                RowKey = PropertyRender.Render(_options.RowKey, record),
-            };
-            var propertiesAdded = new HashSet<string>(entity.Properties.Keys);
-            if (record.ParsedData != null)
-            {
-                foreach (var key in _options.AditionalColumns)
-                {
-                    object value = record.Resolve(key);
-                    if (value != null && propertiesAdded.Add(key))
-                    {
-                        entity[key] = EntityProperty.CreateEntityPropertyFromObject(value);
-                    }
-                }
-            }
-            if (propertiesAdded.Count == 0)
-            {
-                entity["RawData"] = new EntityProperty(record.RawData);
-            }
-            return entity;
-        }
-    }
+			public RoundRobinUploader(CloudStorageAccount account, string[] names, bool debug)
+			{
+				_account = account;
+				_names = names;
+				_list = new TCollection[_names.Length];
+				_debug = debug;
+			}
+
+			public abstract TCollection SetupCollection(string name);
+
+			public abstract bool Upload(TCollection col, LogRecord record);
+
+			public bool Upload(LogRecord record)
+			{
+				if (_list.Length == 0) return true;
+
+				for (int i = 0; i < _list.Length; ++i)
+				{
+					_index = (_index + 1) % _list.Length;
+					try
+					{
+						if (_list[_index] == null)
+						{
+							_list[_index] = SetupCollection(_names[_index]);
+						}
+						if (_list[_index] != null)
+						{
+							if (Upload(_list[_index], record))
+							{
+								return true;
+							}
+							_list[_index] = null;
+						}
+					}
+					catch (Exception e)
+					{
+						if (_debug) Trace.TraceError(e.ToString());
+						_list[_index] = null;
+					}
+				}
+				return false;
+			}
+		}
+
+		private class TableUploader : RoundRobinUploader<CloudTable>
+		{
+			private string _partitionKey;
+			private string _rowKey;
+			private HashSet<string> _aditionalColumns;
+
+			public TableUploader(CloudStorageAccount account, string[] names,
+				string partitionKey, string rowKey, HashSet<string> aditionalColumns,
+				bool debug)
+				: base(account, names, debug)
+			{
+				_partitionKey = partitionKey;
+				_rowKey = rowKey;
+				_aditionalColumns = aditionalColumns;
+			}
+
+			public override CloudTable SetupCollection(string name)
+			{
+				var table = _account.CreateCloudTableClient()
+					.GetTableReference(name);
+				table.CreateIfNotExists();
+				return table;
+			}
+
+			public override bool Upload(CloudTable table, LogRecord record)
+			{
+				var entity = ConvertToEntity(record);
+				var result = table.Execute(TableOperation.InsertOrReplace(entity));
+				var statusGroup = result.HttpStatusCode / 100;
+				if (statusGroup != 2 && statusGroup != 4)
+				{
+					if (_debug) Trace.TraceError("HTTP status: " + result.HttpStatusCode);
+					return false;
+				}
+				return true;
+			}
+
+			private DynamicTableEntity ConvertToEntity(LogRecord record)
+			{
+				var entity = new DynamicTableEntity
+				{
+					PartitionKey = PropertyRender.Render(_partitionKey, record),
+					RowKey = PropertyRender.Render(_rowKey, record),
+				};
+				var propertiesAdded = new HashSet<string>(entity.Properties.Keys);
+				if (record.ParsedData != null)
+				{
+					foreach (var key in _aditionalColumns)
+					{
+						object value = record.Resolve(key);
+						if (value != null && propertiesAdded.Add(key))
+						{
+							entity[key] = EntityProperty.CreateEntityPropertyFromObject(value);
+						}
+					}
+				}
+				if (propertiesAdded.Count == 0)
+				{
+					entity["RawData"] = new EntityProperty(record.RawData);
+				}
+				return entity;
+			}
+		}
+
+		private class QueueUploader : RoundRobinUploader<CloudQueue>
+		{
+			public QueueUploader(CloudStorageAccount account, string[] queueNames, bool debug)
+				: base(account, queueNames, debug) { }
+
+			public override CloudQueue SetupCollection(string name)
+			{
+				var queue = _account.CreateCloudQueueClient()
+					.GetQueueReference(name);
+				queue.CreateIfNotExists();
+				return queue;
+			}
+
+			public override bool Upload(CloudQueue queue, LogRecord record)
+			{
+				var lean = new LogRecord
+				{
+					Origin = record.Origin,
+					EventTimestamp = record.EventTimestamp,
+					RawData = record.RawData ?? "",
+				};
+				queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(lean)));
+				return true;
+			}
+		}
+	}
 }
